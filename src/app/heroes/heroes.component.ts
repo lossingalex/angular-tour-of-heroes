e3afac744287eeb0fc5f94c34c0813e9618679c3
Alex Lossing <lossing.alex@gmail.com>
2020-01-15 12:10:25 +0800

    tuto commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,45 @@
+import { HeroService } from './../hero.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  // selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+        // this.getHeroes();
+      });
+  }
+
+}
